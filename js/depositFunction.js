223796e7fe1e2e9aa284775b40eef5570ace9574
unknown <gausulislam5@gmail.com>
2023-09-19 00:43:52 +0600

    update function

--- js/depositFunction.js
+++ js/depositFunction.js
@@ -1,3 +1,26 @@
+
+function getInputFieldValueId(inputFieldId){
+    const inputField=document.getElementById(inputFieldId);
+    const inputFieldValueString=inputField.value;
+    const inputFieldValue=parseFloat(inputFieldValueString);
+    inputField.value='';
+    return inputFieldValue;
+}
+
+function getElementValueById(elementId){
+    const element=document.getElementById(elementId);
+    const elementValueString=element.innerText;
+    const elementValue=parseFloat(elementValueString);
+    return elementValue
+}
+
+
 document.getElementById('btn-deposit').addEventListener('click',function(){
-    console.log("deposit button");
+    // console.log("deposit button");
+
+    // const newDepositAmount=getInputFieldValueId('deposit-field');
+    
+    // const newDepositAmount=getInputFieldValueId('deposit-field');
+
+  
 })
